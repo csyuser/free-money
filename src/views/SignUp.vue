ea8cb3a3b402332163e47c7dc7ed57179bb8911a
csyuser <13540039093@163.com>
2020-10-20 18:05:17 +0800

    项目初始化

---
+++ src/views/SignUp.vue
@@ -0,0 +1,17 @@
+<template>
+<div class="signUp">
+  <van-button type="primary">主要按钮</van-button>
+  signUp
+</div>
+</template>
+
+<script>
+export default {
+name: "Signup"
+}
+
+</script>
+
+<style scoped>
+
+</style>
