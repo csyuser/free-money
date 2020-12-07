72faac4ff796f6944f61cbc44ee988e9d49f7dea
csyuser <13540039093@163.com>
2020-12-07 17:06:24 +0800

    添加add页面

--- src/views/Add.vue
+++ src/views/Add.vue
@@ -1,33 +1,72 @@
 <template>
   <div class="add">
-    <Nav navTitle="添加日记">
-      <Editor :init="init"></Editor>
+    <Nav navTitle="添加日记" iconName="save" @navIconClicked="save">
+      <van-form>
+        <van-field
+            v-model="diaryInfo.title"
+            name="标题"
+            placeholder="标题"
+            :rules="[{ required: true, message: '请填写标题' }]"
+        />
+        <van-field readonly clickable name="picker" :value="diaryInfo.tagTitle" placeholder="点击选择标签"
+            @click="showPicker = true"
+        />
+        <van-popup v-model="showPicker" position="bottom">
+          <van-picker show-toolbar :columns="columns" @confirm="onConfirm" @cancel="showPicker = false"/>
+        </van-popup>
+        <van-field type="textarea" v-model="diaryInfo.content" name="内容" placeholder="内容"
+        />
+      </van-form>
     </Nav>
   </div>
 </template>
 
 <script>
 import Nav from '@/components/Nav'
-import Editor from '@tinymce/tinymce-vue'
+
 
 export default {
   name: 'Add',
-  components: {Nav,Editor},
+  components: {Nav},
   data(){
     return{
-      init: {
-        branding: false,
-        // minHeight: 300,
-        selector: 'textarea',
-        mobile: {
-          menubar: true,
-          theme: 'mobile',
-          plugins: 'autosave lists autolink',
-          toolbar: 'undo bold italic styleselect'
-        }
-      }
+      diaryInfo:{
+        title:'',
+        content:'',
+        tagTitle:'',
+        tagValue:''
+      },
+      columns:[],
+      showPicker: false,
     }
   },
+  mounted() {
+    this.axios.get('/tag/list').then(res => {
+      if (res.data['Code'] === 0) {
+        res.data['Res'].sort((a, b) => a.Type - b.Type)
+        const result = []
+        console.log(res.data)
+        res.data['Res'].forEach(item=>{
+          result.push({text:item['Title'],id:item['Id']})
+        })
+        this.columns = result
+        console.log(this.columns)
+      } else {this.$toast.fail(res.data['Msg'])}
+    })
+        .catch()
+  },
+  methods:{
+    save() {
+      this.axios.post('notebook/create',{...this.diaryInfo})
+      console.log(this.diaryInfo)
+    },
+    onConfirm(value) {
+      console.log(value)
+      this.diaryInfo.tagTitle = value.text;
+      this.diaryInfo.tagValue = value.id;
+      this.showPicker = false;
+    },
+  }
 }
 </script>
 
